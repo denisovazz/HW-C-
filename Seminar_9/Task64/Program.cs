0d6e1d27e7a6ec740ed8ac5e897b5803a392e428
Denisova Olga <denisovazz77@gmail.com>
2023-10-26 22:12:00 +0300

    Task64

---
+++ Seminar_9/Task64/Program.cs
@@ -0,0 +1,21 @@
+﻿int N = InputNumbers("Введите N: ");
+
+PrintNature(N);
+
+void PrintNature(int n)
+{
+    if (n == 0)
+    {
+        return;
+    }
+
+    Console.Write(n+" ");
+    PrintNature(n-1);
+}
+
+int InputNumbers(string input) 
+{
+  Console.Write(input);
+  int output = Convert.ToInt32(Console.ReadLine());
+  return output;
+}
