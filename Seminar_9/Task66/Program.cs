c89e1c9c75bb4f7e8ca56b1a83c3d735874ddb1a
Denisova Olga <denisovazz77@gmail.com>
2023-10-26 22:40:18 +0300

    Task66

---
+++ Seminar_9/Task66/Program.cs
@@ -0,0 +1,21 @@
+﻿int M = InputNumbers("Введите M: ");
+int N = InputNumbers("Введите N: ");
+
+System.Console.WriteLine(Summ(M,N));
+
+int Summ(int m, int n)
+{
+    if (m > n)
+    {
+        return 0;
+    }
+    
+    return n + Summ(m, n-1);
+}
+
+int InputNumbers(string input) 
+{
+  Console.Write(input);
+  int output = Convert.ToInt32(Console.ReadLine());
+  return output;
+}
